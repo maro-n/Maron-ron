0a7786ea47b33ade5fc17e911c2d9021fab6578c
maro-n <with.tennis.win.0406@gmail.com>
2014-05-24 17:20:45 +0900

    Hello x10

--- hello cygwin.c
+++ hello cygwin.c
@@ -2,10 +2,9 @@
 
 int main()
 {
-	int i = 0;
-	for (i = 0; i < 10; i++)
-{
-		printf("Hello World C Language\n");
-}	
-	return 0;
+    int i;
+    for (i = 0; i < 10; i++){
+        printf("Hello World C Language\n");
+    }	
+    return 0;
 }
